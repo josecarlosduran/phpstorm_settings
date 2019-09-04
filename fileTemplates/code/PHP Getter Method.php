548830466aaceb5d567c9e91599e9e12b7d3015c
Jose Carlos Duran <josecarlos.duran@eurona.com>
2019-09-04 09:38:26 +0200

    PS-2019.2.1 <jduran@ERN-00446 Create PHP Class.php, ide.general.xml, PHP Getter Method.php, PHP Constructor.php, laf.xml, Default.xml, filetypes.xml, php.xml, PHP Interface.php, colors.scheme.xml, PHP Trait.php, debugger.xml, updates.xml

---
+++ fileTemplates/code/PHP Getter Method.php
@@ -0,0 +1,9 @@
+
+public ${STATIC} function ${NAME}()#if(${RETURN_TYPE}): ${RETURN_TYPE}#else#end
+{
+#if (${STATIC} == "static")
+    return self::$${FIELD_NAME};
+#else
+    return $this->${FIELD_NAME};
+#end
+}
---
+++ fileTemplates/internal/PHP Trait.php
@@ -0,0 +1,12 @@
+<?php
+#parse("PHP File Header.php")
+declare (strict_types=1);
+#if (${NAMESPACE})
+
+namespace ${NAMESPACE};
+
+#end
+
+trait ${NAME} {
+
+}
